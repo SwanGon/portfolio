0229ea7ccf50a5e770584c237e1b5fc29f6b0721
Swan GONZALES <E204031Y@ubuntu.srv-podman-2>
2024-02-12 15:14:07 +0100

    correction ébauche

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,11 +14,11 @@ interface NavItem {
 
 const NAV_ITEMS: Array<NavItem> = [
   {
-    label: "Home",
+    label: "Accueil",
     page: "home",
   },
   {
-    label: "About",
+    label: "À propos de moi",
     page: "about",
   },
   {
