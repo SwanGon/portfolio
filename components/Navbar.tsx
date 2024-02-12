f53f475ae483148a9470a41506b91654a71cbc32
Swan GONZALES <E204031Y@ubuntu.srv-podman-2>
2024-02-12 15:15:54 +0100

    correction ébauche

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,10 +21,6 @@ const NAV_ITEMS: Array<NavItem> = [
     label: "À propos de moi",
     page: "about",
   },
-  {
-    label: "Projects",
-    page: "projects",
-  },
 ]
 
 export default function Navbar() {
